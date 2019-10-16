8930a48cf6f06b194938a04dc39c417fb04372f9
Tori Fife <torifife@gmail.com>
2019-10-15 21:00:41 -0600

    just trying to get some file to go in

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="View your information for wewrite account">
+</head>
+
+<body>
+    <header>
+        We Write USA is a writing platform blah blah blah
+    </header>
+
+
+    <footer>
+        Website created by Tori Fife. 10/2019.
+    </footer>
+</body>
+</html>
