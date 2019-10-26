4be89b0af43d23d3770bf7c09defba64db3a985a
Tori Fife <torifife@gmail.com>
2019-10-26 11:37:46 -0600

    test

--- delete-comment.php
+++ delete-comment.php
@@ -8,11 +8,11 @@ $user = $_GET['user'];
 echo $comment . "<br>";
 echo $user . "<br>";
 
-/*if ($user == $_SESSION['user_id']) {
+if ($user == $_SESSION['user_id']) {
     $statement = $db->prepare("DELETE FROM comment WHERE comment_id = $comment AND user_id = $user");
     $statement->execute();
     echo("successful");
-}*/
+}
 
 //$location = $_SERVER['HTTP_REFERER'];
 //header("Location: $location");
